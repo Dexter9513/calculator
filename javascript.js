c50ba599f51aba716e9fce1cba4adc86c4edf217
Dexter <dextermorgan9513@gmail.com>
2022-05-21 22:14:38 +0545

    more functions added like square root brackets

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const buttons = document.querySelectorAll('button');
 
 let inputs = [];
 let resetFlag = false;
+let ans = 0;
 
 function keyboardPress(e) {
     let keyPressed = filterKeyboardPress(e.key);
@@ -14,12 +15,13 @@ function keyboardPress(e) {
 }
 
 function filterKeyboardPress(key) {
+    if (key === 'E') return '×10^';
+    if (key === 'Backspace') return 'DEL';
     key = key.replace('*', '×')
         .replace('/', '÷')
         .replace('-', '−')
         .replace('Enter', '=');
-    if (key.length !== 1) return false;
-    if (key.match(/[0-9+×÷−=\.]/)) {
+    if (key.match(/^[0-9+×÷−=\.\(\)]$/)) {
         return key;
     }
 }
@@ -31,23 +33,39 @@ function buttonPress() {
     handleInput(buttonPressed);
 }
 
-function handleInput(value) {
-    if (resetFlag) {
-        reset();
-        resetFlag = false;
-    }
+function elementIsOperator(value) {
+    if (value.match(/[\+−×÷\^²/]$|×10\^/)) return true;
+}
 
-    if (value === '=') {
+function calculate(expression) {
+    let parsed = parseExpression(expression);
+    return evaluateExpression(parsed);
+}
+
+function handleInput(value) {
+    if (value === 'AC') {
+        reset(true);
+    } else if (value === 'DEL') {
+        inputs.pop();
+        input.textContent = inputs.join('');
+    } else if (value === '=') {
         resetFlag = true;
-        expression = parseExpression(inputs);
-        if (expression) {
-            evaluateExpression(expression);
+        let result = calculate(inputs);
+        if (result) {
+            displayResult(result);
         } else {
             displayResult('Math Error!');
         }
-    } else if (!['Ans', '×10', '(', ')'].includes(value)){
-        input.textContent += value;
+    } else {
+        if (resetFlag) {
+            reset();
+            resetFlag = false;
+            if (elementIsOperator(value)) {
+                handleInput('Ans');
+            }
+        }
         inputs.push(value);
+        input.textContent = inputs.join('');
     }
 }
 
@@ -67,76 +85,140 @@ function multiply(a, b) {
     return a * b;
 }
 
+function exponent(a, b) {
+    return String(a ** b);
+}
+
+function square(a) {
+    if (!isNaN(a)) return String(a ** 2);
+}
+
+function squareRoot(a) {
+    if (!isNaN(a)) return String(Math.sqrt(a));
+}
+
 function reduce(array, index, func) {
-    operand1 = array[index - 1];
-    operand2 = array[index + 1];
-    if (isNaN(operand1) || isNaN(operand2)) {
-        return ['Math Error!'];
-    } else {
-        array.splice(index - 1, 3, func(operand1, operand2));
+    let operand1 = array[index - 1];
+    let operand2 = array[index + 1];
+    if (func === square) {
+        let result = square(operand1);
+        if (!result) return ['Math Error!'];
+        array.splice(index - 1, 2, result);
+        return array;
+    } else if (func === squareRoot) {
+        let result = squareRoot(operand2);
+        if (!result) return ['Math Error!'];
+        array.splice(index, 2, result);
+        return array;
+    } else if (func == '−' && !isNaN(operand2)) {
+        if (!isNaN(operand1)) {
+            array.splice(index, 2, '+', String(-operand2));
+        } else {
+            array.splice(index, 2, String(-operand2));
+        }
         return array;
     }
+    if (isNaN(operand1) || isNaN(operand2)) {
+        return ['Math Error!'];
+    }
+    array.splice(index - 1, 3, func(operand1, operand2));
+    return array;
+}
+
+function parseBrackets(expressionArray) {
+    let indexStart = expressionArray.indexOf('(');
+    if (indexStart === -1) {
+        return expressionArray;
+    }
+    let depth = 0;
+    for (let i = indexStart + 1; i < expressionArray.length; i++) {
+        if (expressionArray[i] === '(') {
+            depth += 1;
+        } else if (expressionArray[i] === ')') {
+            if (depth === 0) {
+                let parseResult = parseExpression(expressionArray.slice(indexStart + 1, i));
+                let replaceWith = evaluateExpression(parseResult);
+                expressionArray.splice(indexStart, i + 1 - indexStart, replaceWith);
+                return expressionArray;
+            }
+            depth -= 1;
+        }
+    }
+    return ['Math Error!'];
 }
 
 function parseExpression(inputArray) {
-    const expressionArray = inputArray.join('').match(/([\+−×÷/])|([\d\.]+)/g);
+    let expressionArray = inputArray.join('').match(/([\+−×÷√²\^\(\)/])|([\d\.]+)|(Ans)/g);
+    expressionArray = expressionArray.map(element => element === 'Ans' ? String(ans).replace('-', '−') : element);
+    while (expressionArray.includes('(')) {
+        expressionArray = parseBrackets(expressionArray);
+    }
+    expressionArray = preliminaryEvaluation(expressionArray);
     for (let i = 0; i < expressionArray.length; i++) {
-        element = expressionArray[i];
-        nextElement = expressionArray[i + 1];
-        if (element.match(/[\+−×÷/]/)) {
-            if (element === '−') {
-                if (i === 0) {
-                    expressionArray[1] = -expressionArray[1];
-                    expressionArray.shift();
-                    continue;
-                } else if (!isNaN(nextElement)) {
-                    expressionArray[i] = '+';
-                    expressionArray[i + 1] = '-' + expressionArray[i + 1];
-                    continue;
-                }
-            }
+        let element = expressionArray[i];
+        let nextElement = expressionArray[i + 1];
+        // console.log({ element, nextElement });
+        if (element.match(/[\+×÷/]/)) {
             if (isNaN(nextElement)) {
-                if (nextElement === '−') {
-                    expressionArray.splice(i + 1, 2, '-' + expressionArray[i + 2]);
-                    continue;
-                } else {
-                    return false;
-                }
+                return ['Math Error!'];
             }
         } else if (!isNaN(element)) {
             if (!isNaN(nextElement)) {
-                return false;
+                expressionArray.splice(i + 1, 0, '×');
             }
             expressionArray[i] = parseFloat(expressionArray[i]);
         } else {
-            return false
+            return ['Math Error!'];
         }
     }
+    // console.log(expressionArray);
+    return expressionArray;
+}
+
+function preliminaryEvaluation(expressionArray) {
+    do {
+        if (expressionArray.includes('√')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('√'), squareRoot);
+        } else if (expressionArray.includes('²')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('²'), square);
+        } else if (expressionArray.includes('^')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('^'), exponent);
+        } else if (expressionArray.includes('−')) {
+            expressionArray = reduce(expressionArray, expressionArray.lastIndexOf('−'), '−');
+        } else {
+            break;
+        }
+    } while (true);
     return expressionArray;
 }
 
 function evaluateExpression(expressionArray) {
     while (expressionArray.length != 1) {
-        console.log(expressionArray);
+        // console.log(expressionArray);
         if (expressionArray.includes('÷')) {
             expressionArray = reduce(expressionArray, expressionArray.indexOf('÷'), divide);
         } else if (expressionArray.includes('×')) {
             expressionArray = reduce(expressionArray, expressionArray.indexOf('×'), multiply);
         } else if (expressionArray.includes('+')) {
             expressionArray = reduce(expressionArray, expressionArray.indexOf('+'), add);
-        } else if (expressionArray.includes('−')) {
-            expressionArray = reduce(expressionArray, expressionArray.indexOf('−'), subtract);
         }
     }
-    displayResult(expressionArray[0]);
+    ans = expressionArray[0];
+    return String(ans);
 }
 
-function reset() {
+function reset(resetOutput = false) {
     input.textContent = '';
     inputs = [];
+    if (resetOutput) {
+        output.textContent = '';
+    }
 }
 
 function displayResult(result) {
+    if (!isNaN(result) && String(result).length > 14) {
+        result = parseFloat(result) .toExponential(9);
+    }
     output.textContent = result;
 }
 
