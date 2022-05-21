90bf1bc8712c17dbc803d3146c0a35178efd8e2d
Dexter <dextermorgan9513@gmail.com>
2022-05-21 14:45:54 +0545

    Basic math functions added

--- javascript.js
+++ javascript.js
@@ -2,20 +2,25 @@ const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 const buttons = document.querySelectorAll('button');
 
-const inputs = [];
+let inputs = [];
+let resetFlag = false;
 
 function keyboardPress(e) {
-    // console.log(keyPressed)
     let keyPressed = filterKeyboardPress(e.key);
+    // console.log(keyPressed)
     if (keyPressed) {
-        handleInput(keyPressed)
+        handleInput(keyPressed);
     }
 }
 
 function filterKeyboardPress(key) {
-    key = key.replace('*', '×').replace('/', '÷').replace('-','−').replace('Enter', '=')
-    if (key.match(/[0-9+×÷−=]/)) {
-        return key
+    key = key.replace('*', '×')
+        .replace('/', '÷')
+        .replace('-', '−')
+        .replace('Enter', '=');
+    if (key.length !== 1) return false;
+    if (key.match(/[0-9+×÷−=\.]/)) {
+        return key;
     }
 }
 
@@ -27,16 +32,112 @@ function buttonPress() {
 }
 
 function handleInput(value) {
+    if (resetFlag) {
+        reset();
+        resetFlag = false;
+    }
+
     if (value === '=') {
-        displayResult()
-    } else {
-        input.textContent += value
-        inputs.push(value)
+        resetFlag = true;
+        expression = parseExpression(inputs);
+        if (expression) {
+            evaluateExpression(expression);
+        } else {
+            displayResult('Math Error!');
+        }
+    } else if (!['Ans', '×10', '(', ')'].includes(value)){
+        input.textContent += value;
+        inputs.push(value);
     }
 }
 
-function displayResult() {
-    output.textContent = inputs.join('')
+function divide(a, b) {
+    return a / b;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function reduce(array, index, func) {
+    operand1 = array[index - 1];
+    operand2 = array[index + 1];
+    if (isNaN(operand1) || isNaN(operand2)) {
+        return ['Math Error!'];
+    } else {
+        array.splice(index - 1, 3, func(operand1, operand2));
+        return array;
+    }
+}
+
+function parseExpression(inputArray) {
+    const expressionArray = inputArray.join('').match(/([\+−×÷/])|([\d\.]+)/g);
+    for (let i = 0; i < expressionArray.length; i++) {
+        element = expressionArray[i];
+        nextElement = expressionArray[i + 1];
+        if (element.match(/[\+−×÷/]/)) {
+            if (element === '−') {
+                if (i === 0) {
+                    expressionArray[1] = -expressionArray[1];
+                    expressionArray.shift();
+                    continue;
+                } else if (!isNaN(nextElement)) {
+                    expressionArray[i] = '+';
+                    expressionArray[i + 1] = '-' + expressionArray[i + 1];
+                    continue;
+                }
+            }
+            if (isNaN(nextElement)) {
+                if (nextElement === '−') {
+                    expressionArray.splice(i + 1, 2, '-' + expressionArray[i + 2]);
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        } else if (!isNaN(element)) {
+            if (!isNaN(nextElement)) {
+                return false;
+            }
+            expressionArray[i] = parseFloat(expressionArray[i]);
+        } else {
+            return false
+        }
+    }
+    return expressionArray;
+}
+
+function evaluateExpression(expressionArray) {
+    while (expressionArray.length != 1) {
+        console.log(expressionArray);
+        if (expressionArray.includes('÷')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('÷'), divide);
+        } else if (expressionArray.includes('×')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('×'), multiply);
+        } else if (expressionArray.includes('+')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('+'), add);
+        } else if (expressionArray.includes('−')) {
+            expressionArray = reduce(expressionArray, expressionArray.indexOf('−'), subtract);
+        }
+    }
+    displayResult(expressionArray[0]);
+}
+
+function reset() {
+    input.textContent = '';
+    inputs = [];
+}
+
+function displayResult(result) {
+    output.textContent = result;
 }
 
 window.addEventListener('keydown', keyboardPress);
